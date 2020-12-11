90af9a6a36ae4425265856fd366baaad5930e0b9
MarcGrauPujol <75749590+MarcGrauPujol@users.noreply.github.com>
2020-12-11 15:49:34 +0100

    Add files via upload

---
+++ sql7_freesqldatabase_com.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 4.7.1
+ */
+
+/**
+ * Database `sql7381488`
+ */
+
+/* `sql7381488`.`estudiant` */
+$estudiant = array(
+  array('nom' => 'Alicia Rodriguez','id_estudiant' => '5','PBE' => '1','DSBM' => '1','TD' => '1','RP' => '1','PSAVC' => '1'),
+  array('nom' => 'Barbara Fernandez','id_estudiant' => '4','PBE' => '1','DSBM' => '1','TD' => '1','RP' => '1','PSAVC' => '1'),
+  array('nom' => 'Cecilia Jimenez','id_estudiant' => '3','PBE' => '1','DSBM' => '1','TD' => '1','RP' => '0','PSAVC' => '1'),
+  array('nom' => 'Jordi Bujons','id_estudiant' => '1','PBE' => '1','DSBM' => '1','TD' => '0','RP' => '0','PSAVC' => '0'),
+  array('nom' => 'Marc Grau','id_estudiant' => '2','PBE' => '1','DSBM' => '1','TD' => '0','RP' => '0','PSAVC' => '1')
+);
+
+/* `sql7381488`.`notes` */
+$notes = array(
+  array('assignatura' => 'PBE','nom' => 'Nota final','nota' => '8.6','id_estudiant' => '1'),
+  array('assignatura' => 'PBE','nom' => 'Nota puzzles','nota' => '9.8','id_estudiant' => '1'),
+  array('assignatura' => 'PBE','nom' => 'Nota final','nota' => '8.0','id_estudiant' => '2'),
+  array('assignatura' => 'PBE','nom' => 'Nota puzzles','nota' => '6.9','id_estudiant' => '2'),
+  array('assignatura' => 'PBE','nom' => 'Nota final','nota' => '8.2','id_estudiant' => '3'),
+  array('assignatura' => 'PBE','nom' => 'Nota final','nota' => '8.5','id_estudiant' => '4'),
+  array('assignatura' => 'PBE','nom' => 'Nota final','nota' => '8.4','id_estudiant' => '5'),
+  array('assignatura' => 'PBE','nom' => 'Nota puzzles','nota' => '7.7','id_estudiant' => '5'),
+  array('assignatura' => 'PBE','nom' => 'Nota puzzles','nota' => '7.5','id_estudiant' => '3'),
+  array('assignatura' => 'PBE','nom' => 'Nota puzzles','nota' => '7.9','id_estudiant' => '4'),
+  array('assignatura' => 'DSBM','nom' => 'Examen parcial','nota' => '5.2','id_estudiant' => '1'),
+  array('assignatura' => 'DSBM','nom' => 'Examen Final','nota' => '7.4','id_estudiant' => '1'),
+  array('assignatura' => 'DSBM','nom' => 'Examen parcial','nota' => '6.1','id_estudiant' => '2'),
+  array('assignatura' => 'DSBM','nom' => 'Examen final','nota' => '6.5','id_estudiant' => '2'),
+  array('assignatura' => 'DSBM','nom' => 'Examen parcial','nota' => '3.7','id_estudiant' => '3'),
+  array('assignatura' => 'DSBM','nom' => 'Examen final','nota' => '5.9','id_estudiant' => '3'),
+  array('assignatura' => 'DSBM','nom' => 'Examen parcial','nota' => '8.3','id_estudiant' => '4'),
+  array('assignatura' => 'DSBM','nom' => 'Examen final','nota' => '5.2','id_estudiant' => '4'),
+  array('assignatura' => 'DSBM','nom' => 'Examen parcial','nota' => '1.1','id_estudiant' => '5'),
+  array('assignatura' => 'DSBM','nom' => 'Examen final','nota' => '4.8','id_estudiant' => '5'),
+  array('assignatura' => 'TD','nom' => 'Examen parcial','nota' => '3.1','id_estudiant' => '3'),
+  array('assignatura' => 'TD','nom' => 'Examen final','nota' => '4.0','id_estudiant' => '3'),
+  array('assignatura' => 'TD','nom' => 'Examen parcial','nota' => '2.0','id_estudiant' => '4'),
+  array('assignatura' => 'TD','nom' => 'Examen final','nota' => '0.5','id_estudiant' => '4'),
+  array('assignatura' => 'TD','nom' => 'Examen parcial','nota' => '4.8','id_estudiant' => '5'),
+  array('assignatura' => 'TD','nom' => 'Examen final','nota' => '5.4','id_estudiant' => '5'),
+  array('assignatura' => 'RP','nom' => 'Examen parcial','nota' => '5.2','id_estudiant' => '4'),
+  array('assignatura' => 'RP','nom' => 'Examen final','nota' => '4.8','id_estudiant' => '4'),
+  array('assignatura' => 'RP','nom' => 'Examen parcial','nota' => '3.2','id_estudiant' => '5'),
+  array('assignatura' => 'RP','nom' => 'Examen final','nota' => '5.9','id_estudiant' => '5'),
+  array('assignatura' => 'PSAVC','nom' => 'Mitjana parcials','nota' => '4.2','id_estudiant' => '2'),
+  array('assignatura' => 'PSAVC','nom' => 'Examen final','nota' => '5.1','id_estudiant' => '2'),
+  array('assignatura' => 'PSAVC','nom' => 'Mitjana parcials','nota' => '7.3','id_estudiant' => '3'),
+  array('assignatura' => 'PSAVC','nom' => 'Examen final','nota' => '5.8','id_estudiant' => '3'),
+  array('assignatura' => 'PSAVC','nom' => 'Mitjana parcials','nota' => '2.9','id_estudiant' => '4'),
+  array('assignatura' => 'PSAVC','nom' => 'Examen final','nota' => '1.3','id_estudiant' => '4'),
+  array('assignatura' => 'PSAVC','nom' => 'Mitjana parcials','nota' => '4.1','id_estudiant' => '5'),
+  array('assignatura' => 'PSAVC','nom' => 'Examen final','nota' => '3.0','id_estudiant' => '5')
+);
+
+/* `sql7381488`.`tasques` */
+$tasques = array(
+  array('data' => '15 de Gener','assignatura' => 'PBE','nom' => 'Entrega treball final'),
+  array('data' => '21 de Desembre','assignatura' => 'TD','nom' => 'Examen laboratori'),
+  array('data' => '22 de Desembre','assignatura' => 'RP','nom' => 'Entrega practica 6'),
+  array('data' => '19 de Desembre','assignatura' => 'PSAVC','nom' => 'Entrega practica 5'),
+  array('data' => '10 de Gener','assignatura' => 'RP','nom' => 'Examen final'),
+  array('data' => '18 de Gener','assignatura' => 'PBE','nom' => 'Examen final'),
+  array('data' => '11 de Gener','assignatura' => 'TD','nom' => 'Examen final'),
+  array('data' => '16 de Gener','assignatura' => 'PSAVC','nom' => 'Examen final'),
+  array('data' => '7 de Gener','assignatura' => 'DSBM','nom' => 'Examen final'),
+  array('data' => '18 de Desembre','assignatura' => 'DSBM','nom' => 'Entrega practica C3'),
+  array('data' => '18 de Gener','assignatura' => 'DSBM','nom' => 'Examen de laboratori'),
+  array('data' => '15 de Desembre','assignatura' => 'PSAVC','nom' => 'Entregable numero 18'),
+  array('data' => '13 de Desembre','assignatura' => 'TD','nom' => 'Exercici Atenea')
+);
+
+/* `sql7381488`.`timetables` */
+$timetables = array(
+  array('dia' => 'Dilluns','hora' => '10:00-12:00','assignatura' => 'PSAVC','aula' => 'A4102'),
+  array('dia' => 'Divendres','hora' => '10:00-11:00','assignatura' => 'PSAVC','aula' => 'A4102'),
+  array('dia' => 'Dilluns','hora' => '14:00-16:00','assignatura' => 'PBE','aula' => 'A3202'),
+  array('dia' => 'Dimecres','hora' => '17:00-20:00','assignatura' => 'PBE','aula' => 'A3201'),
+  array('dia' => 'Dimarts','hora' => '8:00-10:00','assignatura' => 'TD','aula' => 'A4201'),
+  array('dia' => 'Divendres','hora' => '12:00-14:00','assignatura' => 'TD','aula' => 'A4201'),
+  array('dia' => 'Dimecres','hora' => '8:00-10:00','assignatura' => 'RP','aula' => 'A2102'),
+  array('dia' => 'Dilluns','hora' => '8:00-10:00','assignatura' => 'RP','aula' => 'A2102')
+);
